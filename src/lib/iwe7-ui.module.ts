f2b60f9f535497b1f0ee56a6f8b24e7f1910fd75
iwe7 <admin@meepo.com.cn>
2018-06-05 17:43:24 +0800

    #6

--- src/lib/iwe7-ui.module.ts
+++ src/lib/iwe7-ui.module.ts
@@ -2,6 +2,9 @@ import { Iwe7BoxModule } from 'iwe7-box';
 import { Iwe7SquareModule } from 'iwe7-square';
 import { Iwe7SwiperModule } from 'iwe7-swiper';
 import { Iwe7BetterScrollModule } from 'iwe7-better-scroll';
+import { Iwe7FlexBoxModule } from 'iwe7-flex';
+import { Iwe7AbcModule } from 'iwe7-abc';
+
 import { NgModule } from '@angular/core';
 import { MatDialogModule, MatIconModule, MatButtonModule } from '@angular/material';
 
@@ -10,6 +13,8 @@ import { MatDialogModule, MatIconModule, MatButtonModule } from '@angular/materi
     Iwe7BetterScrollModule,
     Iwe7SwiperModule,
     Iwe7SquareModule,
+    Iwe7FlexBoxModule,
+    Iwe7AbcModule,
     Iwe7BoxModule,
     MatDialogModule, 
     MatIconModule, 
