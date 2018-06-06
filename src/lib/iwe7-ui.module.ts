2582999c1c273c54bc265e52bcfebbf3ee1272fb
iwe7 <admin@meepo.com.cn>
2018-06-06 10:43:11 +0800

    #18

--- src/lib/iwe7-ui.module.ts
+++ src/lib/iwe7-ui.module.ts
@@ -3,11 +3,9 @@ import { NgModule } from '@angular/core';
 import { Iwe7BoxModule } from 'iwe7-box';
 import { Iwe7SquareModule } from 'iwe7-square';
 import { Iwe7SwiperModule } from 'iwe7-swiper';
-import { Iwe7BetterScrollModule } from 'iwe7-better-scroll';
 import { Iwe7FlexBoxModule } from 'iwe7-flex';
 import { Iwe7AbcModule } from 'iwe7-abc';
 export const Iwe7Modules = [
-  Iwe7BetterScrollModule,
   Iwe7SwiperModule,
   Iwe7SquareModule,
   Iwe7FlexBoxModule,
@@ -16,11 +14,16 @@ export const Iwe7Modules = [
 ];
 
 import {
-  MatDialogModule, MatIconModule,
-  MatButtonModule, MatListModule,
-  MatGridListModule, MatBadgeModule,
-  MatTooltipModule, MatCardModule,
-  MatDividerModule
+  MatDialogModule,
+  MatIconModule,
+  MatButtonModule,
+  MatListModule,
+  MatGridListModule,
+  MatBadgeModule,
+  MatTooltipModule,
+  MatCardModule,
+  MatDividerModule,
+  MatTabsModule
 } from '@angular/material';
 
 export const MaterialModules = [
@@ -32,6 +35,7 @@ export const MaterialModules = [
   MatTooltipModule,
   MatCardModule,
   MatDividerModule,
+  MatTabsModule
 ];
 
 import { ObserversModule } from '@angular/cdk/observers';
